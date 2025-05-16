5fd6b05c357b6f2827961718151cc88f0da255d1
tadaharu1130 <kasai.tadaharu@mirait-one.com>
2025-05-16 10:09:16 +0900

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,17 @@
 import { defineAuth } from "@aws-amplify/backend";
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: "Welcome to Travel Advisor! Verify your email!",
+      verificationEmailBody: (code) => `Here is your verification code: ${code()}`,
+      verificationEmailStyle: "CODE",
+    },
+  },
+  userAttributes: {
+    preferredUsername: {
+      mutable: true,
+      required: true,
+    },
   },
 });
